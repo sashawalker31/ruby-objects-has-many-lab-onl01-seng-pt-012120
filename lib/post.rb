f86130213c345e6dfaa7ef04c55e9e98d0ca1236
sasha walker <sashawalker31@gmail.com>
2020-02-19 23:07:34 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,15 +1,11 @@
-class Post
+class Post 
+  attr_accessor :title, :author 
 
-  attr_accessor :title, :author
   def initialize(title)
     @title = title
   end
 
   def author_name
-    if @author == nil
-      return nil
-    else
-      @author.name
-    end
+    self.author.name
   end
 end
