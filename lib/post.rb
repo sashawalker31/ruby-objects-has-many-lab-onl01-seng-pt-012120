14f0a4d913162d42102fac20020f96a36e65d756
sasha walker <sashawalker31@gmail.com>
2020-02-19 23:09:07 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,22 @@
-class Post 
-  attr_accessor :title, :author 
+class Post
+  attr_accessor :title, :author
+
+  @@all = []
 
   def initialize(title)
     @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def author_name
-    self.author.name
+    if self.author
+      self.author.name
+    else
+      nil
+    end
   end
 end
